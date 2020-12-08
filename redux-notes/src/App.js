70dd546a4cfa54355cbbff0ac427f2cc097f8bfd
ishon19 <pathak.shreyans@gmail.com>
2020-12-08 18:06:03 +0530

    reducer splitted

--- redux-notes/src/App.js
+++ redux-notes/src/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { createStore } from "redux";
+import noteReducer from "./reducers/noteReducer";
+
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "the app state is in redux",
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: "NEW_NOTE",
+  data: {
+    content: "state changes are made with actions",
+    important: false,
+    id: 2,
+  },
+});
 
 const App = () => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.data.id}>
+            {note.data.content} <strong>{note.data.important ? "important" : ""}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;

3a4ff64a477b82694b39886bf85ee40bf3c8d3ec
ishon19 <pathak.shreyans@gmail.com>
2020-12-09 12:21:28 +0530

    components added

--- redux-notes/src/App.js
+++ redux-notes/src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { createNote, toggleImportanceOf } from "./reducers/noteReducer";
+import NewNote from "./components/NewNote";
+import Notes from "./components/Note";
 
 const App = () => {
-  const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
-
-  const addNote = (event) => {
-    event.preventDefault();
-    const content = event.target.note.value;
-    event.target.note.value = "";
-    dispatch(createNote(content));
-  };
-
-  const toggleImportance = (id) => {
-    dispatch(toggleImportanceOf(id));
-  };
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {notes.map((note) => (
-          <li key={note.data.id} onClick={() => toggleImportance(note.data.id)}>
-            {note.data.content}{" "}
-            <strong>{note.data.important ? "important" : ""}</strong>
-          </li>
-        ))}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   );
 };
